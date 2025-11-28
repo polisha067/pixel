f95d934baedce6c462545b0bfe81b19142218b78
Ivan Voskresenskiy <iavoskresenskij@mai.education>
2025-11-28 17:13:28 +0300

    update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, APIRouter, HTTPException
 from models import MailRequest, EmailResponse
+from fastapi.middleware.cors import CORSMiddleware
 from ai_funcs import generate_mail, analyze_mail
 import uvicorn
 
@@ -9,6 +10,15 @@ app = FastAPI()
 router = APIRouter(tags=["MailRequest"])
 
 app.include_router(router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "http://localhost:5500", "http://127.0.0.1:5500", "http://localhost:8000", "http://127.0.0.1:8000", "http://127.0.0.1:8001", "http://localhost:8001"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get('/')
 async def root():
@@ -23,9 +33,10 @@ async def mail_generator(request: MailRequest):
         raise HTTPException(status_code=500, detail=f"Error generating email: {str(e)}")
 
 async def generate_answer(incoming_letter):
-    email_type = await analyze_mail(incoming_letter)
-    r = await generate_mail(incoming_letter, instructions=f"Write a {email_type} letter answering to this, you are employee in the bank PSB.")
-    return r
+    # email_type = await analyze_mail(incoming_letter)
+    # r = await generate_mail(incoming_letter, instructions=f"Write a {email_type} letter answering to this, you are employee in the bank PSB.")
+    # return r
+    return f"Ответ на письмо: {incoming_letter[:100]}..."
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8001)
+    uvicorn.run("main:app", host='127.0.0.1', port=8001)
